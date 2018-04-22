ad2bfce48419c656fcae876c9ab81451d2463675
naordahan <33223417+naordahan@users.noreply.github.com>
2018-04-22 03:02:30 +0300

    Revert "Add files via upload"
    This reverts commit 99b3abf48992eb98418f4765986134cbe05a47b0.

--- CircularInt.hpp
+++ CircularInt.hpp
@@ -5,6 +5,7 @@
 using namespace std;
 
 
+<<<<<<< HEAD
 class CircularInt {
 	int start;
 	int finish; 
@@ -40,3 +41,205 @@ public:
 	CircularInt CircularInt::operator-(int num, CircularInt & c);
 	CircularInt CircularInt::operator-(CircularInt & c, int num);
 #endif 
+=======
+class CircularInt{
+    int start;
+    int finish;
+    int current;
+    
+    //constructors
+    CircularInt (int s, int f){
+        if(s<f){
+            start=s;
+            finish=f;
+        }
+        else{
+            start=f;
+            finish=s;
+        }
+        current= start;
+    }
+    
+    
+     CircularInt (CircularInt &c){//copy constructor
+        start= c.start;
+        finish= c.finish;
+        current= c.current;
+    }
+    
+    
+    
+friend ostream& operator<<(ostream& os, const CircularInt& c);
+friend istream& operator>>(istream& is,  CircularInt& c);
+friend CircularInt operator- (int num, CircularInt& c);
+friend CircularInt operator- ( CircularInt& c, int num);
+ CircularInt operator-= (CircularInt& c, int num);
+
+
+
+//operator +  for two structures
+CircularInt operator+ (const CircularInt& c) 
+{ 
+   CircularInt result {start, finish};
+   result.current= current+c.current;
+   while(result.current>finish){
+       result.current= result.current- finish;
+   }
+   return result;
+} 
+
+//operator + for adding a number to a structure
+CircularInt operator+ (const int n){
+    CircularInt result {start, finish};
+    result.current= current+n;
+    while(result.current>finish){
+       result.current= result.current- finish;
+   }
+    return result;
+}
+
+//operator +=
+CircularInt& operator += (const int num){
+    current= current+ num;
+    while (current>finish){
+        current=current-finish;
+    }
+    return *this;
+}
+
+/*//operator -=
+CircularInt& operator-= (int num){
+    current= current-num;
+    while(current<start){
+        current=current+finish;
+    }
+    return *this;
+}*/
+
+
+//operator ++ Prefix
+CircularInt operator++ (int)
+{
+ CircularInt result(*this);
+ operator++();
+ return *this;
+}
+
+//operator++ Postfix
+CircularInt& operator++(){
+    current++;
+    while (current>finish){
+        current= current-finish;
+    }
+    return *this;
+}
+
+//operator-- Prefix
+CircularInt operator-- (int){
+    CircularInt result (*this);
+    operator--();
+    return *this;
+}
+
+//operator-- Postfix
+CircularInt& operator-- (){
+    current--;
+    while(current<start){
+        current+=finish;
+    }
+    return *this;
+}
+ 
+ //operator - for substracting the number from the maximum possible number
+ CircularInt operator- (){
+     CircularInt result{start,finish};
+     result.current= result.finish-current;
+     while(result.current<result.start){
+         result.current+=result.finish;
+     }
+     return result;
+ }
+ 
+
+ 
+ //operator *=
+ CircularInt operator *= (const int num){
+     current*=num;
+     while (current>finish){
+         current= current-finish;
+     }
+     return *this;
+ }
+ 
+   //operator / finds which numbers can be a divider
+ string operator/ (const int num){
+     string s;
+     for (int i=start; i<=finish; i++){
+         int temp= i*num;
+         while(temp>finish){
+             temp=temp-finish;
+         }
+         if (temp==current){
+             string res= to_string(i);
+             s+= res+ " ";
+         }
+     }
+     if (s.length()==0){
+         s="There is no number x in {1,12} such that x*"+to_string(num)+"=10";
+     }
+     return s;
+ }
+ 
+ 
+ 
+
+};
+ //operator -=
+CircularInt operator-= (CircularInt& c,int num){
+    CircularInt result{c.start,c.finish};
+    result.current= c.current-num;
+    while(result.current<c.start){
+        result.current +=c.finish;
+    }
+    return result;
+}
+
+// operator << 
+  ostream& operator<<(ostream& os, const CircularInt& c)
+{  
+    os << c.current;  
+    return os;  
+} 
+
+// operator >>
+  istream& operator>>(istream& is,  CircularInt& c)
+{  
+    is >> c.current;  
+    return is;  
+} 
+
+
+
+
+//Substracting a structure from a number
+CircularInt operator- (int num, CircularInt& c){
+    CircularInt result {c.start,c.finish};
+    result.current=num- c.current;
+    while(result.current<c.start){
+        result.current+=c.finish;
+    }
+    return result;
+}
+
+//Substracting a number from a structure
+CircularInt operator- ( CircularInt& c, int num){
+    CircularInt result {c.start,c.finish};
+    result.current= c.current-num;
+    while(result.current<c.start){
+        result.current+=c.finish;
+    }
+    return result;
+}
+
+	//using code from https://msdn.microsoft.com/en-us/library/1z2f6c2k.aspx (for operator <<)
+>>>>>>> parent of a960c43... Update CircularInt.hpp
