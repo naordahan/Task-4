dc44a135d5d64835a4ad09555b023737a2bd7569
naordahan <33223417+naordahan@users.noreply.github.com>
2018-04-30 01:45:06 +0300

    Update CircularInt.hpp

--- CircularInt.hpp
+++ CircularInt.hpp
@@ -19,28 +19,31 @@ class CircularInt {
 	CircularInt & operator-();
 	CircularInt & operator-(int start_point);
 	CircularInt & operator-(CircularInt const & c);
+	CircularInt & operator*=(int val);
+	CircularInt & operator+(CircularInt other);
+	CircularInt operator/=(int val);
+	CircularInt & operator/(int num);
+	CircularInt & operator+=(int const & num);
+	CircularInt & operator+=(CircularInt const & c);
+	/*int inline get_start() { return start; }
+	int inline get_finish() { return  finish; }
+	int inline get_current() { return current; }
+	int set_current(int val) { this->current = val; }*/
 	std::string to_string() { return std::to_string(current); }
+	/*int inline get_range() { return get_finish() - get_start() + 1; }*/
 
 
 	//operator overloading:
 
 
 	friend std::ostream& operator<<(std::ostream& os, CircularInt& s) {
-		os << std::to_string(s.current);
+		os << std::to_string(s.get_current());
 		return os;
 	}
 
 	//assuming the range of the two hours is the same
 	//arithmetic operators
-	CircularInt& operator++ (); //prefix ++ operator
-	CircularInt operator-- (int hour1); //Postfix -- operator
-	CircularInt& operator--(); //prefix -- operator
-	CircularInt &operator+=(int const num);
-	CircularInt &CircularInt::operator+=(int const &num)
-	CircularInt & operator*=(int val);
-	CircularInt & operator+(CircularInt other);
-	CircularInt operator/=(int);
-	CircularInt& operator/(int num);
+
 	//logic operators between 2 objects
 	 bool operator<(CircularInt const &c);
 	 bool operator>(CircularInt const &c);
@@ -54,6 +57,10 @@ class CircularInt {
 	 bool operator>=(CircularInt const &c);
 	 bool operator!=(CircularInt  &c);
 	 bool operator!=(int const & c);
+	 CircularInt & operator++();
+	 CircularInt operator++(int);
+	 CircularInt & operator--();
+	 CircularInt operator--(int);
 
 };
  
